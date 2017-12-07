481c3a1f023fb032849ef9ff8a21432c082bc0aa
Lewis Cowles <lewiscowles@me.com>
2017-12-07 14:47:31 +0000

    Updates for Plugin Directory
    * Added two hooks `media-playback-speed-generate-controls` & 'media-playback-speed-data'
    * Modified to use WordPress translation & Escape code
    * Added `readme.txt` and `screenshot-1.png`

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Plugin Name: Media Playback Speed
+ * Description: Appends playback buttons to the Media Player.
+ * Author: Daron Spence
+ * Version: 1.0.0
+ */
+
+add_action( 'wp_enqueue_scripts', function(){
+	wp_enqueue_script( 'jquery' );
+});
+
+add_action( 'wp_footer', function(){
+	$defaults = array(
+		array('rate'=>0.5,'title'=>__('Playback Speed 0.5x','media-playback-speed'),'label'=>__('.5x','media-playback-speed')),
+		array('rate'=>1.0,'title'=>__('Playback Speed 1x','media-playback-speed'),'label'=>__('1x','media-playback-speed')),
+		array('rate'=>1.5,'title'=>__('Playback Speed 1.5x','media-playback-speed'),'label'=>__('1.5x','media-playback-speed')),
+		array('rate'=>2.0,'title'=>__('Playback Speed 2x','media-playback-speed'),'label'=>__('2x','media-playback-speed')),
+	);
+?>
+  <style>
+	.mejs-button.blank-button>button {
+		background: transparent;
+		color: #ccc;
+		font-size: 1em;
+		width: auto;
+	}
+	</style>
+	<script type="text/template" id="playback-buttons-template">
+		<?php if(apply_filters('media-playback-speed-generate-controls', true)): ?>
+			<?php foreach(apply_filters('media-playback-speed-data', $defaults) as $item): ?>
+			<div class="mejs-button blank-button">
+				<button type="button" class="playback-rate-button" data-value="<?php echo esc_attr($item['rate']); ?>" title="<?php echo esc_attr($item['title']); ?>" aria-label="<?php echo esc_attr($item['title']); ?>" tabindex="0"><?php echo esc_html($item['label']); ?></button>
+			</div>
+			<?php endforeach; ?>
+		<?php endif; ?>
+	</script>
+	<script type="text/javascript">
+		(function($){
+			$(window).load( function(){
+
+				var $buttons = $( $("#playback-buttons-template").html() );
+				var $els = $( '.mejs-container' );
+
+				for ( i = 0; i < $els.length; i++ ){
+					var audioTag = $( $els[i] ).find('audio,video')[0];
+					$buttons.find('.playback-rate-button').attr('aria-controls', audioTag.id );
+					var $controls = $( $els[i] ).find('.mejs-controls');
+					if($controls.length > 0) {
+						$controls.find('.mejs-duration-container').after( $buttons.clone() );
+					}
+				}
+				$('body').on('click', '.playback-rate-button', function() {
+					var btnEl = $(this),
+							mediaTag = $('#'+btnEl.attr('aria-controls'))[0],
+							rate = btnEl.attr('data-value');
+					mediaTag.setPlaybackRate(rate);
+				});
+
+			});
+		})(jQuery);
+	</script>
+<?php
+}, 1, 100 );
