b3e647d5319cfa46c37ebb222579135156fc2286
Lewis Cowles <lewiscowles@me.com>
2019-11-09 15:02:48 +0000

    Kill dependency on jQuery
    Tested on test site, this entirely replaces jQuery.
    It's not gorgeous code, in-fact it could maybe do with a cleanup run.
    It is vanilla-js.
    
    I still need to test it on ie 11

--- index.php
+++ index.php
@@ -3,12 +3,8 @@
  * Plugin Name: Media Playback Speed
  * Description: Appends playback buttons to the Media Player. Updated original by Daron Spence.
  * Author: LewisCowles
- * Version: 1.0.3
+ * Version: 1.0.4
  */
-
-add_action( 'wp_enqueue_scripts', function(){
-	wp_enqueue_script( 'jquery' );
-});
 
 add_action( 'wp_footer', function(){
 	$defaults = array(
@@ -36,36 +32,47 @@ add_action( 'wp_footer', function(){
 		<?php endif; ?>
 	</script>
 	<script type="text/javascript">
-		(function($){
-			$(window).load( function(){
+		(function() {
+			var buttons = document.createRange().createContextualFragment(
+				document.querySelector("#playback-buttons-template").innerHTML
+			);
+			var els = [].slice.call( document.querySelectorAll( '.mejs-container' ) );
 
-				var $buttons = $( $("#playback-buttons-template").html() );
-				var $els = $( '.mejs-container' );
+			els.forEach(function(elem, i) {
+				var mediaTag = elem.querySelector('audio,video');
+				[].slice.call(
+					buttons.querySelectorAll('.playback-rate-button')
+				).forEach(function(elem) {
+					elem.setAttribute('aria-controls', mediaTag.id );
+				})
+				var controls = elem.querySelector('.mejs-controls');
+				if(controls) {
+					var container =controls.querySelector('.mejs-duration-container');
+					container.parentNode.insertBefore(buttons.cloneNode(true), container.nextSibling);
 
-				for ( i = 0; i < $els.length; i++ ){
-					var mediaTag = $( $els[i] ).find('audio,video')[0];
-					$buttons.find('.playback-rate-button').attr('aria-controls', mediaTag.id );
-					var $controls = $( $els[i] ).find('.mejs-controls');
-					if($controls.length > 0) {
-						$controls.find('.mejs-duration-container').after( $buttons.clone() );
-						$(mediaTag).on('loadedmetadata', function() {
-							var activeSpeed = $(this).closest('.wp-playlist').find('.mejs-container .playback-rate-button.mejs-active');
-								rate = activeSpeed.attr('data-value');
-							$(this)[0].setPlaybackRate(rate);
-						});
-					}
+					mediaTag.addEventListener('loadedmetadata', function(e) {
+						var activeSpeed = e.target.closest('.wp-playlist').querySelector('.mejs-container .playback-rate-button.mejs-active');
+							rate = activeSpeed.dataset.value;
+						e.target.setPlaybackRate(rate);
+					});
 				}
-				$('body').on('click', '.playback-rate-button', function() {
-					var btnEl = $(this),
-							mediaTag = $('#'+btnEl.attr('aria-controls'))[0],
-							rate = btnEl.attr('data-value');
-					mediaTag.setPlaybackRate(rate);
-
-					$(this).closest('.mejs-container').find('.playback-rate-button').removeClass('mejs-active');
-					$(this).addClass('mejs-active');
-				});
 			});
-		})(jQuery);
+			document.body.addEventListener('click', function(e) {
+				if(!e.target || !e.target.classList.contains('playback-rate-button')) { return; }
+
+				var btnEl = e.target,
+						mediaTag = document.querySelector(`#${e.target.getAttribute('aria-controls')}`),
+						rate = e.target.dataset.value;
+				mediaTag.setPlaybackRate(rate);
+
+				[].slice.call(
+					mediaTag.closest('.mejs-container').querySelectorAll('.playback-rate-button')
+				).map(function(elem) {
+					elem.classList.remove('mejs-active');
+				});
+				e.target.classList.add('mejs-active');
+			});
+		})();
 	</script>
 <?php
 }, 1, 100 );
